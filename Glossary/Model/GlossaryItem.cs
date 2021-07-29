aa62f64e9a4509a5c5cc3acf0d5d60bfbed07911
user <user@DESKTOP-1U72J75>
2021-07-29 09:45:15 +0100

    Add project files.

---
+++ Glossary/Model/GlossaryItem.cs
@@ -0,0 +1,10 @@
+﻿//GlossaryItem.cs
+
+namespace Glossary
+{
+    public class GlossaryItem
+    {
+        public string Term { get; set; }
+        public string Definition { get; set; }
+    }
+}
